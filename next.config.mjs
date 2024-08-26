46068d54f95a541271955d25811216624095ba0f
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-08-25 23:05:52 -0300

    chore: allow images from api domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "wefit-react-web-test.s3.amazonaws.com",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
