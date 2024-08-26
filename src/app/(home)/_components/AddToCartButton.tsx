9523d8c5c68a572a7e735adba094b533a7087510
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-08-26 14:39:54 -0300

    feat: add state for when item is already on cart

--- src/app/(home)/_components/AddToCartButton.tsx
+++ src/app/(home)/_components/AddToCartButton.tsx
@@ -3,20 +3,31 @@
 import { Button } from "@/components/Button"
 import { Product } from "@/domain/Product"
 import { useCartContext } from "@/features/cart/cart-context"
+import Image from "next/image"
 
 type AddToCartButtonProps = {
   product: Product
 }
 export function AddToCartButton({ product }: AddToCartButtonProps) {
-  const { addToCart } = useCartContext()
+  const { addToCart, cart } = useCartContext()
+  const itemInCart = cart.items.find((item) => item.id === product.id)
 
   return (
     <Button
       onClick={() => {
-        console.log("product :>> ", product)
         addToCart(product)
       }}
+      variant={itemInCart ? "success" : "default"}
     >
+      <span className="text-xs font-normal flex gap-1">
+        <Image
+          src="/assets/cart-plus.svg"
+          width={14}
+          height={14}
+          alt="Ícone de carrinho"
+        />
+        {itemInCart?.quantity ?? 0}
+      </span>
       ADICIONAR AO CARRINHO
     </Button>
   )
