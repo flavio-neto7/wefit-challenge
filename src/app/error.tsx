a9075e49c06069280ea17c11881c9e4931f53194
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-08-25 22:15:33 -0300

    feat: call listmovies and add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Button } from "@/components/Button"
+import Image from "next/image"
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div className="flex flex-col justify-center items-center bg-white p-16 gap-6 text-center rounded">
+      <h2 className="text-surface font-bold text-xl">
+        Aconteceu um erro ao carregar a página :(
+      </h2>
+      <Image
+        src="/assets/reload-page.png"
+        alt="Recarregar a página"
+        width={178}
+        height={265}
+      />
+      <Button onClick={() => reset()}>Recarregar página</Button>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { listMovies } from "./_actions/list-movies"
+
+export default async function Home() {
+  const movies = await listMovies()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home</h1>
