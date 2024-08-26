f3fe4bd5521f8eb58d0b38e0ee715a49c18bb931
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-08-26 13:58:05 -0300

    feat: add link to home on navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link"
 import { Cart } from "./Cart"
 
 export function Navbar() {
   return (
     <nav className="px-4 py-8 flex justify-between items-center">
-      <h1 className="text-xl font-bold">WeMovies</h1>
+      <Link href="/">
+        <h1 className="text-xl font-bold">WeMovies</h1>
+      </Link>
       <Cart />
     </nav>
   )
