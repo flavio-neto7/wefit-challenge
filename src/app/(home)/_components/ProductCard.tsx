2ae620e11fe9071a0f0d9cb369799db90cea56ba
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-08-25 23:05:26 -0300

    feat: add product card component

---
+++ src/app/(home)/_components/ProductCard.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import { ListMoviesItem } from "../_schema"
+import { Button } from "@/components/Button"
+import { PriceFormatter } from "@/lib/formatters"
+
+type ProductCardProps = {
+  product: ListMoviesItem
+}
+
+export function ProductCard({ product }: ProductCardProps) {
+  return (
+    <div className="rounded p-4 bg-white text-center flex flex-col justify-center items-center space-y-2">
+      <Image src={product.image} alt={product.title} width={147} height={188} />
+      <h3 className="text-heading text-xs font-bold">{product.title}</h3>
+      <p className="text-surface font-bold text-base">
+        {PriceFormatter.format(product.price)}
+      </p>
+      <Button>ADICIONAR AO CARRINHO</Button>
+    </div>
+  )
+}
