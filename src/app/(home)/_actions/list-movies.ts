29ac77b682a33fa884efd63f64fdd3cf342fd38c
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-08-25 22:41:43 -0300

    feat: add button variants using cva

--- src/app/(home)/_actions/list-movies.ts
+++ src/app/(home)/_actions/list-movies.ts
@@ -15,6 +15,8 @@ const listMoviesSchema = z.object({
 })
 
 export const listMovies = cache(async () => {
+  // fake wait
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   const fetchMoviesListResponse = await fetch(process.env.MOVIES_API_URL!)
   if (!fetchMoviesListResponse.ok) {
     throw new Error("Failed to fetch movies list")
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,30 @@
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import { cn } from "@/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center font-bold text-xs py-3 px-6 gap-3 rounded whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground",
+        success: "bg-success text-success-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   children: React.ReactNode
 }
 
-export function Button(props: ButtonProps) {
+export function Button({ className, variant, ...props }: ButtonProps) {
   return (
-    <button className="inline-flex items-center justify-center bg-primary font-bold text-xs py-3 px-6 gap-3 rounded">
+    <button className={cn(buttonVariants({ variant, className }))}>
       {props.children}
     </button>
   )
