d3d6e0e1edb2b6919e7dd71958bbb9a0c353e8ef
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-08-25 23:06:08 -0300

    feat: display product cards inside grid

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { listMovies } from "./_actions/list-movies"
 import { EmptyState } from "./_components/EmptyState"
+import { ProductCard } from "./_components/ProductCard"
 
 export default async function Home() {
   const movies = await listMovies()
@@ -7,8 +8,12 @@ export default async function Home() {
   if (!movies.length) return <EmptyState />
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div></div>
+    <main className="flex min-h-screen flex-col items-center justify-between pb-4">
+      <div className="grid gap-4">
+        {movies.map((movie) => (
+          <ProductCard key={movie.id} product={movie} />
+        ))}
+      </div>
     </main>
   )
 }
